c3906c8050039aebc2d9c1cdb83d43a12ca47505
xxm0703 <xxm0703@abv.bg>
2018-08-29 14:26:51 +0300

    Some new scripts

--- PyScripts/change.py
+++ PyScripts/change.py
@@ -3,6 +3,8 @@ l = [1, 2, 5, 10, 20, 50]
 
 
 def change(amount, coins):
+    if (amount, coins) in calc.keys():
+	 return calc[(amount, coins)]
     if coins == 0:
         if amount == 0:
             return 1
@@ -11,13 +13,10 @@ def change(amount, coins):
     n = s = 0
 
     while amount - n * l[coins - 1] >= 0:
-        if ((amount - n * l[coins - 1]), coins - 1) in calc.keys():
-            s += calc[((amount - n * l[coins - 1]), coins - 1)]
-        else:
-            s += change(amount - n * l[coins - 1], coins - 1)
+	s += change(amount - n * l[coins - 1], coins - 1)
         n += 1
     calc[(amount, coins)] = s
     return s
 
 
-print(change(5, 6))
+print(change(10000, 6))
